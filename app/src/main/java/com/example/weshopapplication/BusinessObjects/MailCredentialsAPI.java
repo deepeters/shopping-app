3115d8772d2eac0f73b5c992adc51f58c055d88d
Dennis Peters <dennis95peters@gmail.com>
2021-06-09 21:28:29 +0300

    The commit pupulates the database with sample products

--- app/src/main/java/com/example/weshopapplication/BusinessObjects/MailCredentialsAPI.java
+++ app/src/main/java/com/example/weshopapplication/BusinessObjects/MailCredentialsAPI.java
@@ -2,6 +2,6 @@ package com.example.weshopapplication.BusinessObjects;
 
 
 public class MailCredentialsAPI {
-    public static final String EMAIL_ADDRESS = "sabinlungu293@gmail.com"; // The test e-mail address to send to.
-    public static final String PASSWORD = "sphysiyjrfcjkpnt"; // The password.
+    public static final String EMAIL_ADDRESS = "dester1219@gmail.com"; // The test e-mail address to send to.
+    public static final String PASSWORD = "gitiri5991"; // The password.
 }
